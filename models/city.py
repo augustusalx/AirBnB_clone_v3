1c7630aa77dcf42302361c5810e9edd8f4b45c94
Augustusalx <Augustusemmanuel91@gmail.com>
2023-05-29 16:15:47 -0700

    commits

---
+++ models/city.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+"""It holds class City"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class City(BaseModel, Base):
+    """The representation of city """
+    if models.storage_t == "db":
+        __tablename__ = 'cities'
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities")
+    else:
+        state_id = ""
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """will initialize city"""
+        super().__init__(*args, **kwargs)
